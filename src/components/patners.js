17feb532cec5179f1a0372f2d4f17b2509643485
ZAIN ALI <zaincui5542@gmail.com>
2024-01-23 01:25:35 +0500

    Comitted by me

--- src/components/patners.js
+++ src/components/patners.js
@@ -8,7 +8,7 @@ export default function Patners() {
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
             <img
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-              src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Microsoft_logo_%282012%29.svg/1024px-Microsoft_logo_%282012%29.svg.png"
               alt="Transistor"
               width={158}
               height={48}
